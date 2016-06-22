92af1c8a09384949b04edb3b0124771a7cc2a4b0
nbertrand <n.bertrand69@gmail.com>
2016-06-22 11:22:29 +0200

    Première version, reste des erreurs de calculs, pas de tests

---
+++ extreme-carpaccio.test/ClassTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace extreme_carpaccio.test
+{
+    public class Test
+    {
+        [Test]
+        public void DoSometing()
+        {
+            Assert.IsFalse(false);
+            //Assert.That(true,Is.False);
+        }
+    }
+}
